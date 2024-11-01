bfbe8cc48900ed8ec18edda7b4b9f3b72a8175b2
ruhaans05 <143101275+ruhaans05@users.noreply.github.com>
2024-11-01 15:00:05 -0400

    Create prices.py

---
+++ prices.py
@@ -0,0 +1,37 @@
+#Will update infrequently
+
+target_prices = {
+            "earpods": 19.99,
+            "tide pods": 7.89,
+            "gg eggs": 6.99,
+            "oikos greek yogurt": 4.59,
+            "good and gather eggs": 6.99,
+            "good and gather salad": 3.59,
+            "gg salad": 3.59,
+            "gg pork (per lb)": 3.19,
+            "good and gather pork (per lb)": 3.19,
+            "gg water (24 pack)": 3.59,
+            "good and gather water (24 pack)": 3.59,
+            "gg water (32 pack)": 5.19,
+            "good and gather water (32 pack)": 5.19,
+            "gg water (1 gallon)": 1.39,
+            "good and gather water (1 gallon)": 1.39,
+            "gg bananas": 2.19,
+            "good and gather bananas": 2.19,
+            "gg beef (per lb)": 3.19,
+            "good and gather beef (per lb)": 3.19,
+            "tyson chicken breast": 11.16,
+            "gg small chicken breast": 8.34,
+            "good and gather small chicken breast": 8.34,
+            "gg large chicken breast": 13.45,
+            "good and gather large chicken breast": 13.45,
+            "shredded chicken": 7.99,
+            "tyson chicken nuggets": 8.29,
+            "chicken fajitas": 7.99,
+            "chicken parmesan": 11.99,
+            
+            
+        }
+
+
+
